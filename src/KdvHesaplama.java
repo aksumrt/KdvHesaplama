519ff81551d79dd27caa39574d5e01efd48bad8e
Mert AKSU <mert.aksuu96@gmail.com>
2021-07-07 22:47:22 +0300

    0-1000 TL %18 KDV 1000< %8 KDV

--- src/KdvHesaplama.java
+++ src/KdvHesaplama.java
@@ -2,18 +2,45 @@ import java.util.Scanner;
 
 public class KdvHesaplama {
     public static void main(String[] args) {
-        double kdvOrani = 0.18 , kdvsizFiyat , kdvliFiyat , kdv ;
+        double kdvOrani1 = 0.18 , kdvOrani2 = 0.08 , kdvsizFiyat , kdvliFiyat , kdv = 0 ;
+
+        Scanner inp = new Scanner(System.in);
 
         System.out.println("Lütfen KDVsiz Fiyati giriniz :");
-        Scanner inp = new Scanner(System.in);
-        kdvsizFiyat = inp.nextDouble();
-        kdv = kdvOrani * kdvsizFiyat ;
-        System.out.print("KDV tutari: ");
-        System.out.println(kdv);
 
-        kdvliFiyat = kdv + kdvsizFiyat;
-        System.out.print("KDVli fiyat");
-        System.out.println(kdvliFiyat);
+        kdvsizFiyat = inp.nextDouble();
+
+        if(kdvsizFiyat<=1000 && kdvsizFiyat>0)
+        {
+            kdv = kdvOrani1 * kdvsizFiyat;
+            System.out.print("KDV tutari: ");
+            System.out.println(kdv);
+
+            kdvliFiyat = kdv + kdvsizFiyat;
+            System.out.print("KDVli fiyat :" + kdvliFiyat);
+          //  System.out.println(kdvliFiyat)
+
+        }
+        else if(1000<kdvsizFiyat)
+        {
+            kdv = kdvOrani2 * kdvsizFiyat;
+            System.out.print("KDV tutari: ");
+            System.out.println(kdv);
+
+            kdvliFiyat = kdv + kdvsizFiyat;
+            System.out.print("KDVli fiyat :" + kdvliFiyat);
+         //   System.out.println(kdvliFiyat)
+
+        }
+        else
+        {
+            System.out.println("Doğru değerleri girdiğinizden emin olunuz.");
+        }
+
+
+
+
+
 
     }
 }
